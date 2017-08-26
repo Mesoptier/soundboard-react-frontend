29dee8cd72e43d943fd24be7770c08f9d444cd7b
Koen Klaren <koen.klaren@gmail.com>
2017-08-26 20:54:29 +0200

    Add api

---
+++ src/api.js
@@ -0,0 +1,14 @@
+// @flow
+type Sample = {
+    id: string,
+    path: string,
+    name: string,
+    mtime: number,
+    categories: string[],
+};
+
+export async function fetchSamples(): Promise<Sample[]> {
+    const response = await fetch('https://viller.men/soundboard-api/samples');
+    const data = await response.json();
+    return data.samples;
+}
