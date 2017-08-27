bcdf668175caef8157bb3c73126f55e156b0c970
Koen Klaren <koen.klaren@gmail.com>
2017-08-27 13:21:42 +0200

    Use arrow function for mapStateToProps

--- src/containers/SampleContainer.ts
+++ src/containers/SampleContainer.ts
@@ -3,10 +3,8 @@ import { connect } from 'react-redux';
 import SampleContainer from '../components/SampleContainer';
 import { State } from '../redux';
 
-function mapStateToProps(state: State) {
-    return {
-        samples: state.samples.samples,
-    };
-}
+const mapStateToProps = (state: State) => ({
+    samples: state.samples.samples,
+});
 
 export default connect(mapStateToProps)(SampleContainer);
