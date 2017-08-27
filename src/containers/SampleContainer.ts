3ed789ebeeb3ae491c97dbc3c7618e0ebee5c78f
Koen Klaren <koen.klaren@gmail.com>
2017-08-27 13:49:03 +0200

    Filter samples on query

--- src/containers/SampleContainer.ts
+++ src/containers/SampleContainer.ts
@@ -2,9 +2,10 @@ import { connect } from 'react-redux';
 
 import SampleContainer from '../components/SampleContainer';
 import { State } from '../redux';
+import { getVisibleSamples } from '../redux/samples/selectors';
 
 const mapStateToProps = (state: State) => ({
-    samples: state.samples.samples,
+    samples: getVisibleSamples(state),
 });
 
 export default connect(mapStateToProps)(SampleContainer);
---
+++ src/redux/samples/selectors.ts
@@ -0,0 +1,21 @@
+import { createSelector } from 'reselect';
+import { State } from '../';
+import { Sample } from '../../api';
+
+export const getSamples = (state: State) => state.samples.samples;
+export const getQuery = (state: State) => state.samples.query;
+
+export const getVisibleSamples = createSelector(
+    getSamples,
+    getQuery,
+    (samples: Sample[], query: string) => {
+        if (query === '') {
+            return samples;
+        }
+
+        return samples.filter((sample) => {
+            return sample.name.includes(query)
+                || sample.categories.some(category => category.includes(query));
+        });
+    },
+);
