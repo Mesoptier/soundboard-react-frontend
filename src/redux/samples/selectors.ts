94f97af019cde6fe65499f013fef548a037e1afd
Koen Klaren <koen.klaren@gmail.com>
2017-09-01 21:21:48 +0200

    Use more advanced search logic from old backend

--- src/redux/samples/selectors.ts
+++ src/redux/samples/selectors.ts
@@ -5,6 +5,28 @@ import { Sample } from '../../api';
 export const getSamples = (state: State) => state.samples.samples;
 export const getQuery = (state: State) => state.samples.query;
 
+const sampleMatchesQuery = (query: string) => {
+    const terms = query
+        // Strip non-alphanumeric characters (will be done in target as well)
+        .replace(/[^\w\s\|]/g, '')
+        // Enable OR-searching when whitespace is around the pipe character "|"
+        .replace(/\s+\|\s+/g, '|')
+        // Split by any combination of whitespace characters
+        .split(/[\s\+&]+/g);
+
+    const regex = new RegExp(
+        `.*${terms.map(term => `(?=.*${term}.*)`).join('')}.*`,
+        'i',
+    );
+
+    return (sample: Sample): boolean =>
+        regex.test(
+            [sample.name, ...sample.categories]
+                .map(item => item.replace(/[^\w\s\|]/g, ''))
+                .join(' '),
+        );
+};
+
 export const getVisibleSamples = createSelector(
     getSamples,
     getQuery,
@@ -13,11 +35,6 @@ export const getVisibleSamples = createSelector(
             return samples;
         }
 
-        return samples.filter(sample => {
-            return (
-                sample.name.includes(query) ||
-                sample.categories.some(category => category.includes(query))
-            );
-        });
+        return samples.filter(sampleMatchesQuery(query));
     },
 );
